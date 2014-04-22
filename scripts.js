31eaf0f7940cc2ea488ddc5b420e925a423be134
br2417 <br2417@columbia.edu>
2014-04-22 12:07:58 -0400

    scripts file updated after 22.04.14 class

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ var myTableURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+
 		
 		gDataTable.addColumn('string', UNEMPDATA.columns[0]);  //only works because this is a google.visualization
 		gDataTable.addColumn('number', UNEMPDATA.columns[1]);
+		gDataTable.addColumn({type: 'string', 'role': 'tooltip', 'p': {'html': true}});
 		
 		gDataTable.addRows(UNEMPDATA.rows) //only works because this is a google.visualization
 		
@@ -75,13 +76,16 @@ var myTableURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+
 		 */
 		 
 		 var chartOptions = {
-          title: "Unemployment since 1948"
+          title: "Unemployment since 1948",
+          tooltip: { isHtml: true } 
         };
 
 
 		//tell it to create a line chart
 		var myChart = new google.visualization.LineChart(document.getElementById("myChartDiv"));
   			myChart.draw(gDataTable, chartOptions);
+  			
+  			
    	
 	}
 
@@ -94,6 +98,8 @@ var myTableURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+
 	var myYear = myNameArray[1]; //grab the year
 	
 	$.get(myTableURL+"'"+myYear+"-12-01'"+myKey, dataLoaded, "json");
+	
+	History.pushState({year:myYear}, "Unemployment from - "+myYear, "?year="+myYear);
 }
 
 	
@@ -102,9 +108,34 @@ var myTableURL = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+
 		
 		console.log("google loaded");
 		
+		//start with URL - URL is just a string; when I split it on '?' I'm doing ...
+		var myURL = History.getState().cleanUrl;
+
+		var queryArray = myURL.split("?"); //split the URL on ?, into two pieces
+		
+		var defaultYear = "1990";
+		
+		//current year that you have in the URL 
+		
+		//if query array length is great than 1 then take default year 
+		//query array = 1, is the year 1990
+		// the next thing that happens, if you split on = sign, then you have two strings, "year" and "1990"
+		//and then saying get the thing in position 1 which is 1990 
+		//and the default year happens to also be 1990 
+		
+		if(queryArray.length > 1){
+			// the below is not great practice bc you have to think through what is going on 
+			//get the query string, break it on equals and then take the right half that contains the year
+			defaultYear = queryArray[1].split("=")[1];
+		}
+		
+		//console.log(cleanURL);
+		
 		$('.btn-success').on("click", showNewData);
 		
-		$("#year_1990").click();
+		//grab the button with the id that is year_"default
+		
+		$("#year_"+defaultYear).click();
 		
 		//  * instead of loading data from a static JSON file I'm going to laotd it from a google fusion table
 		//$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+12A0eKUXKb4TCFhngfaGqaMEH6uECY1iBhd3VOBAV&key=AIzaSyDsBrSpJRliKgi913vr9FWTy8oL57c42bA", dataLoaded, "json");
